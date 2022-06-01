cf53a2d7773ca06f2498340951e930c6d4df3c6d
rafay-ah <54492363+rafay-ah@users.noreply.github.com>
2022-06-01 17:12:58 +0500

    Fixed up Everything

--- cogcc_extraction.py
+++ cogcc_extraction.py
@@ -7,16 +7,15 @@ data = pd.read_excel('Active_Operators.xlsx')
 data = data.drop_duplicates(subset=['Operator Number'], keep="first")
 company_ids = list(data["Operator Number"])
 
-
 base_url = "https://cogcc.state.co.us/cogis/"
 
-COLUMN_NAMES = ['Company Name', 'Operator Number', 'Address', 'Phone', 'Fax', 'Emergency', 'Employee Last Name',
-                'Employee First Name', 'Email']
+COLUMN_NAMES = ['Company Name', 'Operator Number', 'Address', 'Phone', 'Fax', 'Emergency','Cell','Dispatch', 'Employee Last Name',
+                'Employee First Name', 'Email','Phone Type','Number']
 results = pd.DataFrame(columns=COLUMN_NAMES)
-urls, cleaned_urls, company_name, operator_number, address, phone, fax,emergency,lastname,firstname,email,raw = ([] for i in range(12))
+urls, cleaned_urls, company_name, operator_number, address, phone, fax,dispatch,cell, emergency, lastname, firstname, email, phonetype,employee_number, raw,  = ([] for i in range(16))
 employee_detail_table = ''
 
-for i in company_ids[:25]:
+for i in company_ids[10:45]:
     payload = {'company': i,
                'company_name_number': 'number'}  # data that will be encoded as form data sent with post request
 
@@ -32,7 +31,7 @@ for i in company_ids[:25]:
 for link in cleaned_urls:
     get_employees_data_request = requests.get(link)
     response = str(get_employees_data_request.text)
-    employee_data_response = BeautifulSoup(response, "html.parser")
+    employee_data_response = BeautifulSoup(response, "lxml")
 
     # convert to string and try splitting on /n & /r
     employees_data = employee_data_response.find('blockquote').text
@@ -43,58 +42,98 @@ for link in cleaned_urls:
     # company detail cleaning
     company_name.append(str(employees_data[2]).split('\xa0')[0].strip())
     operator_number.append(str(employees_data[2]).split('\xa0')[2].strip())
-    address.append(re.sub(' +', ' ',employees_data[3]).replace('\r',''))
+    address.append(re.sub(' +', ' ', employees_data[3]).replace('\r', ''))
 
-    #cleaning company contact details
+    # cleaning company contact details
     contact_details = employee_data_response.find('blockquote')
     contact_details = contact_details.contents[11].string
-    contact_details = str(contact_details).replace('\n','').replace('\r','').replace('\t','').strip().split(' ')
+    contact_details = str(contact_details).replace('\n', '').replace('\r', '').replace('\t', '').strip().split(' ')
     strphone = ' '
     stremergency = ' '
     strfax = ' '
+    strcell=''
+    strdispatch=''
     for contact in contact_details:
         if contact.__contains__('PHONE'):
             strphone = contact.replace('PHONE', '').strip()
         elif contact.__contains__('EMERGENCY'):
-            stremergency= contact.replace('EMERGENCY', '').strip()
+            stremergency = contact.replace('EMERGENCY', '').strip()
         elif contact.__contains__('FAX'):
             strfax = contact.replace('FAX', '').strip()
+        elif contact.__contains__('DISPATCH'):
+            strdispatch = contact.replace('DISPATCH', '').strip()
+        elif contact.__contains__('CELL'):
+            strcell = contact.replace('CELL', '').strip()
     phone.append(strphone)
     emergency.append(stremergency)
     fax.append(strfax)
+    cell.append(strcell)
+    dispatch.append(strdispatch)
+
 
     # working on alternative approach for cleaning employee details
     if employee_data_response.find('table') is None:
         lastname.append(' ')
         firstname.append(' ')
         email.append(' ')
+        phonetype.append(' ')
+        employee_number.append(' ')
         continue
     raw_response = employee_data_response.find('table').text
     try:
-        employee_information = raw_response.split('Number',1)[1]
+        employee_information = raw_response.split('Number', 1)[1]
     except IndexError:
         # will be triggered when there are no employees of a company
         lastname.append(' ')
         firstname.append(' ')
         email.append(' ')
+        phonetype.append(' ')
+        employee_number.append(' ')
         continue
-    employee_information = str(employee_information).replace('\n','').replace('\r','').replace('\t','').replace('*','').replace('\xa0',' ').strip()
+    employee_information = str(employee_information).replace('\r', '').replace('\t', '').replace('*', '').replace(
+        '\xa0', ' ').strip()
     employee_information = re.sub(' +', ' ', employee_information)
-    employee_information = employee_information.split(' ')
-    for i in range(0, len(employee_information), 4):
-        if i + 2 <= len(employee_information)-1:
-            firstname.append(employee_information[i])
-            lastname.append(employee_information[i + 1])
-            if employee_information[i + 2].__contains__('@'): # making certain email is not null
-                email.append(employee_information[i + 2])
-            elif  i + 3 <= len(employee_information)-1 and employee_information[i + 3].__contains__('@') :
-                email.append(employee_information[i + 3])# checking for email on next index
-            else: # no email was provided
-                email.append(' ')
-        elif i+1 <len(employee_information): # w'll be triggered when email is missing for employee
-            firstname.append(employee_information[i])
-            lastname.append(employee_information[i + 1])
-            email.append(' ')
+    employee_information = re.sub('(\n)\\1{2,}', '\n', employee_information)
+    employee_information = employee_information.split('\n')
+
+    str_firstname, str_lastname, str_email, str_phonetype, str_number = [' '] * 5
+    for i in range(0, len(employee_information), 5):
+        if i + 2 <= len(employee_information) - 1:
+            # extracting first_name
+            if employee_information[i].isupper():
+                str_firstname = str_firstname + employee_information[i] + ' '
+            else:
+                str_firstname = str_firstname + ' '
+            # extracting last_name
+            if employee_information[i + 1].isupper():
+                str_lastname = str_lastname + employee_information[i + 1] + ' '
+            else:
+                str_lastname = str_lastname + ' '
+            # [extracting email]
+            if employee_information[i + 2].__contains__('@'):
+                str_email = str_email + employee_information[i + 2] + ' '
+            else:  # no email was provided
+                str_email = str_email + ' '
+
+            # [extracting phone type and number]
+            if i + 4 <= len(employee_information) - 1 and employee_information[i + 3].isupper() and employee_information[i + 4].__contains__('('):
+                str_phonetype = str_phonetype + employee_information[i + 3] + ' '
+                str_number = str_number + employee_information[i + 4] + ' '
+            else:  # no email was provided
+                str_phonetype = str_phonetype + ' '
+                str_number = str_number + ' '
+
+        elif i + 1 < len(employee_information):  # w'll be triggered when email/other details are missing for employee
+            str_firstname = str_firstname + employee_information[i] + ' '
+            str_lastname = str_firstname + employee_information[i + 1] + ' '
+            str_email = str_email + ' '
+            str_phonetype = str_phonetype + ' '
+            str_number = str_number + ' '
+    firstname.append(str_firstname)
+    lastname.append(str_lastname)
+    email.append(str_email)
+    phonetype.append(str_phonetype)
+    employee_number.append(str_number)
 
 results[COLUMN_NAMES[0]] = company_name
 results[COLUMN_NAMES[1]] = operator_number
@@ -102,8 +141,12 @@ results[COLUMN_NAMES[2]] = address
 results[COLUMN_NAMES[3]] = phone
 results[COLUMN_NAMES[4]] = fax
 results[COLUMN_NAMES[5]] = emergency
-results[COLUMN_NAMES[6]] = lastname
-results[COLUMN_NAMES[7]] = firstname
-results[COLUMN_NAMES[8]] = email
+results[COLUMN_NAMES[6]] = cell
+results[COLUMN_NAMES[7]] = dispatch
+results[COLUMN_NAMES[8]] = firstname
+results[COLUMN_NAMES[9]] = lastname
+results[COLUMN_NAMES[10]] = email
+results[COLUMN_NAMES[11]] = phonetype
+results[COLUMN_NAMES[12]] = employee_number
 results["Raw HTML"] = raw
 results.to_csv('extracted_Data.csv', sep=',', encoding=False)
